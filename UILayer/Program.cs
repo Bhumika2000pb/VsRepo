1a11e7b766b127d7ba293c97e49d50863d1924c5
Bhumika2000pb <bhumika2000pb@gmail.com>
2023-11-30 00:37:13 +0530

    newchange

--- UILayer/Program.cs
+++ UILayer/Program.cs
@@ -11,6 +11,7 @@ namespace UILayer
         public static void Main(string[] args)
         {
             Console.WriteLine("welcome to Devops tool with modification");
+            show();
             Console.ReadKey();
         }
 
