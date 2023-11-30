a3a149b4ca04319b95b142122e3266843958b464
Bhumika2000pb <bhumika2000pb@gmail.com>
2023-11-30 08:04:00 +0530

    newchange

---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using UILayer;
+using System.IO;
+
+namespace TestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        int res;
+        [TestMethod]
+        public void TestSquare()
+        {
+            //StringWriter w = new StringWriter();
+            //Console.SetOut(w);
+            int res=Program.square(2);
+            Assert.AreEqual(4, res);
+        }
+    }
+}
